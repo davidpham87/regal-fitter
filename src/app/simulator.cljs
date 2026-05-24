54391516d3a0375a2f2dbda0872bae0b879632d9
David Pham <davidpham87@gmail.com>
2026-05-24 15:02:12 +0200

    Fix loop variable closure capture in submit-simulation-jobs! to show all families

--- src/app/simulator.cljs
+++ src/app/simulator.cljs
@@ -21,36 +21,54 @@
       (js/console.error "Stage 1 Error:" e)
       (throw e))))
 
-(defn- submit-simulation-jobs! [config all-accepted families results completed total start-time]
+(defn- submit-simulation-jobs! [config all-accepted families results completed
+                                 total start-time]
   (wp/clear-queue!)
   (if (= total 0)
     (swap! state/app-state assoc :status :done :view :results)
-    (doseq [fam families
-            [idx rec] (map-indexed vector (get all-accepted (keyword fam)))]
-      (wp/submit-job!
-       {:rec rec :cfg-dict config :n-sims (:n-sims-per-combo config) :seed (+ (:seed config) (* idx 7919))}
-       (fn [{:keys [success? result error]}]
-         (swap! completed inc)
-         (swap! state/app-state assoc-in [:progress :completed] @completed)
-         (when (and success? result) (swap! results update (keyword fam) (fnil conj []) result))
-         (when (= @completed total)
-           (log (str "All simulations done in " (/ (- (js/Date.now) start-time) 1000) "s"))
-           (swap! state/app-state assoc :status :done :results @results :view :results)))))))
+    (doseq [fam families]
+      (let [fam-kw (keyword fam)]
+        (doseq [[idx rec] (map-indexed vector (get all-accepted fam-kw))]
+          (wp/submit-job!
+           {:rec rec
+            :cfg-dict config
+            :n-sims (:n-sims-per-combo config)
+            :seed (+ (:seed config) (* idx 7919))}
+           (fn [{:keys [success? result error]}]
+             (swap! completed inc)
+             (swap! state/app-state assoc-in
+                    [:progress :completed] @completed)
+             (when (and success? result)
+               (swap! results update fam-kw (fnil conj []) result))
+             (when (= @completed total)
+               (log (str "All simulations done in "
+                         (/ (- (js/Date.now) start-time) 1000) "s"))
+               (swap! state/app-state assoc :status :done
+                                            :results @results
+                                            :view :results)))))))))
 
 (defn start-simulation! []
   (let [config (:config @state/app-state)
         families (:families config)]
-    (swap! state/app-state assoc :status :running-stage1 :results {} :error-message nil)
+    (swap! state/app-state assoc :status :running-stage1
+                                 :results {}
+                                 :error-message nil)
     (go
       (<! (timeout 50))
       (try
         (let [all-accepted (reduce (fn [acc fam]
                                      (log (str "Running Stage 1 for " fam))
                                      (let [accepted (run-stage1! fam config)]
-                                       (log (str fam " accepted: " (count accepted)))
-                                       (assoc acc (keyword fam) accepted))) {} families)
+                                       (log (str fam " accepted: "
+                                                 (count accepted)))
+                                       (assoc acc (keyword fam) accepted)))
+                                   {} families)
               total-combos (reduce + (map count (vals all-accepted)))]
-          (swap! state/app-state assoc :status :running-stage2 :progress {:total total-combos :completed 0})
-          (submit-simulation-jobs! config all-accepted families (atom {}) (atom 0) total-combos (js/Date.now)))
+          (swap! state/app-state assoc :status :running-stage2
+                                       :progress {:total total-combos
+                                                  :completed 0})
+          (submit-simulation-jobs! config all-accepted families (atom {})
+                                    (atom 0) total-combos (js/Date.now)))
         (catch js/Error e
-          (swap! state/app-state assoc :status :error :error-message (.-message e)))))))
+          (swap! state/app-state assoc :status :error
+                                       :error-message (.-message e)))))))
