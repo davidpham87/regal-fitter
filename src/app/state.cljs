d013016becc3ca13cdbdc89125bb528a833fc16b
David Pham <davidpham87@gmail.com>
2026-05-25 22:52:52 +0200

    Update

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -8,10 +8,13 @@
   {:n-total 126
    :n-per-arm 63
    :enroll-bands
-   [[0.0 12.0 15]
-    [12.0 24.0 50]
-    [24.0 36.0 56]
-    [36.0 38.0 5]]
+   [[0.0 12.0 12]
+    [12.0 18.0 6]
+    [18.0 24.0 9]
+    [24.0 30.0 15]
+    [24.0 28.0 24]  ;; midpoint
+    [28.0 34.0 39]  ;; total should be 105 here
+    [34.0 38.0 21]] ;; 126
 
    :t-ia 46.0
    :t-upd 58.0
@@ -25,28 +28,28 @@
    :prefilter-tol-ia 2.5
    :prefilter-tol-upd 2.5
    :prefilter-tol-pr3 2.5
-   :tol-ia 2.0
-   :tol-upd 2.0
-   :tol-pr3 3.0
+   :tol-ia 1.5
+   :tol-upd 1.5
+   :tol-pr3 1.5
 
-   :tol-increment-ia-upd 5.0
-   :tol-increment-upd-pr3 4.0
+   :tol-increment-ia-upd 3
+   :tol-increment-upd-pr3 3
 
-   :futility-hr-max 0.9
+   :futility-hr-max 0.85
    :efficacy-hr-min 0.00
 
-   :pool-mos-min-at-ia 13.5
+   :pool-mos-min-at-ia 12
    :median-fu-target 13.5
    :median-fu-tol 3.0
 
-   :enforce-no-80-by-today false
+   :enforce-no-80-by-today true
    :no-80-slack-months 1.0
-   :bat-strat-bin 1.0
+   :bat-strat-bin 0.5
 
    :hr-threshold 0.636
 
    :n-sims-per-combo 500
-   :n-sims-screen 100
+   :n-sims-screen 20
    :n-screen-min-pass 1
    :seed 20260508
 
@@ -74,6 +77,7 @@
   (assoc default-config
          :bat-med-grid [8 22 2]
          :bat-shape-grid [0.7 1.0 0.15]
+         :bat-strat-bin 1
          :gps-med-grid-n 3
          :n-sims-per-combo 100
          :n-sims-screen 10
@@ -149,44 +153,13 @@
    :futility-hr-max 1.0
    :pool-mos-min 12.0
    :enroll-bands
-   [[0.0 1.0 2]
-    [1.0 2.0 2]
-    [2.0 3.0 2]
-    [3.0 4.0 2]
-    [4.0 5.0 3]
-    [5.0 6.0 3]
-    [6.0 7.0 3]
-    [7.0 8.0 3]
-    [8.0 9.0 3]
-    [9.0 10.0 3]
-    [10.0 11.0 4]
-    [11.0 12.0 4]
-    [12.0 13.0 4]
-    [13.0 14.0 4]
-    [14.0 15.0 4]
-    [15.0 16.0 4]
-    [16.0 17.0 4]
-    [17.0 18.0 4]
-    [18.0 19.0 4]
-    [19.0 20.0 4]
-    [20.0 21.0 4]
-    [21.0 22.0 4]
-    [22.0 23.0 4]
-    [23.0 24.0 4]
-    [24.0 25.0 4]
-    [25.0 26.0 4]
-    [26.0 27.0 4]
-    [27.0 28.0 4]
-    [28.0 29.0 3]
-    [29.0 30.0 3]
-    [30.0 31.0 3]
-    [31.0 32.0 3]
-    [32.0 33.0 3]
-    [33.0 34.0 3]
-    [34.0 35.0 2]
-    [35.0 36.0 2]
-    [36.0 37.0 2]
-    [37.0 38.0 4]]
+   [[0.0 12.0 12]
+    [12.0 18.0 6]
+    [18.0 24.0 9]
+    [24.0 30.0 15]
+    [24.0 28.0 24]  ;; midpoint
+    [28.0 34.0 39]  ;; total should be 105 here
+    [34.0 38.0 21]]
    :t-ia 46.0
    :t-upd 58.0
    :t-pr3 62.97
