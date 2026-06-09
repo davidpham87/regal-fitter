7991ecfea9cb545a297c3784dbf9141f7946c87b
David Pham <davidpham87@gmail.com>
2026-06-09 23:31:04 +0200

    Create app.components.editor namespace with code-editor component

---
+++ src/app/components/editor.cljs
@@ -0,0 +1,15 @@
+(ns app.components.editor
+  (:require [reagent.core :as r]
+            ["@monaco-editor/react" :default MonacoEditor]))
+
+(defn code-editor
+  [{:keys [value on-change language theme height read-only?]}]
+  [:div.border.rounded-lg.overflow-hidden
+   {:style {:height (or height "400px")}}
+   [:> MonacoEditor
+    {:height "100%"
+     :defaultLanguage (or language "json")
+     :theme (or theme "light")
+     :options {:readOnly (boolean read-only?)}
+     :value value
+     :onChange on-change}]])
