3f9c2bef9ce5faf326a7b21a5ae5708b8d87314d
David Pham <davidpham87@gmail.com>
2026-06-19 01:13:42 +0200

    Swap editor to plain textarea on blur and load Monaco editor only on focus

--- src/app/components/editor.cljs
+++ src/app/components/editor.cljs
@@ -4,12 +4,29 @@
 
 (defn code-editor
   [{:keys [value on-change language theme height read-only?]}]
-  [:div.border.rounded-lg.overflow-hidden
-   {:style {:height (or height "400px")}}
-   [:> MonacoEditor
-    {:height "100%"
-     :defaultLanguage (or language "json")
-     :theme (or theme "light")
-     :options {:readOnly (boolean read-only?)}
-     :value value
-     :onChange on-change}]])
+  (r/with-let [editing? (r/atom false)]
+    (if (and @editing? (not read-only?))
+      [:div.border.rounded-lg.overflow-hidden
+       {:style {:height (or height "400px")}}
+       [:> MonacoEditor
+        {:height "100%"
+         :defaultLanguage (or language "json")
+         :theme (or theme "light")
+         :options {:readOnly false}
+         :value value
+         :onChange on-change
+         :onMount (fn [editor]
+                    (js-invoke editor "onDidBlurEditorText"
+                               (fn []
+                                 (reset! editing? false))))}]]
+      [:textarea.w-full.p-2.font-mono.text-sm.border.rounded-lg
+       {:style {:height (or height "400px")
+                :resize "none"}
+        :value value
+        :readOnly (boolean read-only?)
+        :on-focus (fn []
+                    (when-not read-only?
+                      (reset! editing? true)))
+        :on-change (fn [e]
+                     (when on-change
+                       (on-change (.. e -target -value))))}])))
